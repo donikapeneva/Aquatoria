cc1a5ac1d652059eac67ee5f33d0adb035962342
Donika Peneva <donyka111@abv.bg>
2016-12-20 15:28:22 +0200

    Click event unbinded

--- public/scripts/user/login.js
+++ public/scripts/user/login.js
@@ -1,4 +1,4 @@
-(() => {
+(function () {
     const ALPHA_PATTERN = /^[A-Za-zА-Яа-я1-9]+$/,
         EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         MIN_NAME_LENGTH = 2,
@@ -8,12 +8,24 @@
         $loginButton = $('#login-button'),
         $loginFormErrorContainer = $('#error-container');
 
-    $loginButton.on('click', () => {
+    //TODO: this doesnt solve the problem!!!
+    // var event = jQuery.Event( "submit" );
+    $loginButton.unbind('click');
+    //     .click(function() {
+    //     // alert("bob");
+    //     //addToCart($(this).attr("id"));
+    // });
+
+    $loginButton.on('click', function(event){
+
+        event.preventDefault();
+
+        console.log('on click');
         resetErrorContainer();
 
-        let isFormValid = validateRegistrationForm();
+        let isFormValid = validateLoginForm();
 
-        if(isFormValid) {
+        if (isFormValid) {
             return Promise.resolve()
                 .then(() => {
                     let dataArray = $loginForm.serializeArray(),
@@ -43,15 +55,28 @@
                     let errorObj = JSON.parse(err.responseText);
                     displayValidationErrors(errorObj.message, $loginFormErrorContainer);
                 });
+
         } else {
             //TODO
             displayValidationErrors('Please, enter a valid values', $loginFormErrorContainer);
         }
+
+        event.preventDefault();
+
+        // this.submit();
+
     });
+
+    // $loginButton.on('click', function (e) {
+    //     console.log('onClick --- ');
+    //     e.preventDefault();
+    //     $loginForm.submit();
+    // })
+
 
     function resetErrorContainer() {
         $loginFormErrorContainer.find('p').html('');
-        $loginFormErrorContainer.find('p').hide();
+        $loginFormErrorContainer.find('p').remove();
     }
 
     function displayValidationErrors(message, container) {
@@ -62,12 +87,12 @@
         );
     }
 
-    function validateRegistrationForm() {
+    function validateLoginForm() {
         let isFormValid = false,
             isEmailValid = false,
             isPasswordValid = false;
 
-        $loginForm.find('input').each(function() {
+        $loginForm.find('input').each(function () {
             let input = $(this),
                 inputName = input.attr('name');
 

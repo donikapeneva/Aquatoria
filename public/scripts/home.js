86b85cbfd5a8d7072bd7a3edaa9abca7ee619a9e
Donika <donyka111@abv.bg>
2017-03-22 15:21:50 +0200

    Home buttons

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -5,56 +5,52 @@
     //     $loginButton = $('#login-button'),
     //     $loginFormErrorContainer = $('#error-container');
 
-    $('.wrapper.triangle.top').on('click', function () {
-        location.href = '/home';
-    });
+    $('.wrapper.triangle.top')
+        .on('click', function () {
+            location.href = '/home';
+        })
+        .hover(function () {
+                $('div.container.top').css('visibility', 'visible').fadeIn(500);
+            },
+            function () {
+                $('.container.top').css('visibility', 'hidden').fadeOut(100);
+            });
 
-    $('.wrapper.triangle.right').on('click', function () {
-        location.href = '/items/photos';
-    });
+    $('.wrapper.triangle.right')
+        .on('click', function () {
+            location.href = '/items/photos';
+        })
+        .hover(
+            function () {
+                $('div.container.right').css('visibility', 'visible').fadeIn(500);
+            },
+            function () {
+                $('.container.right').css('visibility', 'hidden').fadeOut(100);
+            });
 
-    $('.wrapper.triangle.bottom').on('click', function () {
-        location.href = '/home';
-    });
 
-    $('.wrapper.triangle.left').on('click', function () {
-        location.href = '/home';
-    });
+    $('.wrapper.triangle.bottom')
+        .on('click', function () {
+            location.href = '/home';
+        })
+        .hover(
+            function () {
+                $('div.container.bottom').css('visibility', 'visible').fadeIn(500);
+            },
+            function () {
+                $('.container.bottom').css('visibility', 'hidden').fadeOut(100);
+            });
 
-    $('.wrapper.triangle.top').hover(
-        function () {
-            $('div.container.top').css('visibility', 'visible').fadeIn( 500 );
-            console.log('i dsjikfdif');
-        },
-        function () {
-            $('.container.top').css('visibility', 'hidden').fadeOut(100);
-        });
-
-    $('.wrapper.triangle.right').hover(
-        function () {
-            $('div.container.right').css('visibility', 'visible').fadeIn( 500 );
-            console.log('i dsjikfdif');
-        },
-        function () {
-            $('.container.right').css('visibility', 'hidden').fadeOut(100);
-        });
-
-    $('.wrapper.triangle.bottom').hover(
-        function () {
-            $('div.container.bottom').css('visibility', 'visible').fadeIn( 500 );
-            console.log('i dsjikfdif');
-        },
-        function () {
-            $('.container.bottom').css('visibility', 'hidden').fadeOut(100);
-        });
-
-    $('.wrapper.triangle.left').hover(
-        function () {
-            $('div.container.left').css('visibility', 'visible').fadeIn( 500 );
-            console.log('i dsjikfdif');
-        },
-        function () {
-            $('.container.left').css('visibility', 'hidden').fadeOut(100);
-        });
+    $('.wrapper.triangle.left')
+        .on('click', function () {
+            location.href = '/home';
+        })
+        .hover(
+            function () {
+                $('div.container.left').css('visibility', 'visible').fadeIn(500);
+            },
+            function () {
+                $('.container.left').css('visibility', 'hidden').fadeOut(100);
+            });
 
 })();
